cebf4f81afcbc7314c32b52fcd1acf8d95300d91
Noam Musba <noammu@wix.com>
2024-11-06 18:58:47 +0200

    asaa

--- app/routes/asdasd.$parammmmmm/route.tsx
+++ app/routes/asdasd.$parammmmmm/route.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLoaderData } from '@remix-run/react';
 import type { LoaderFunctionArgs } from '@remix-run/node';
+import styles from './route.module.scss';
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
     return { params };
@@ -9,7 +10,7 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 const Asdasdparammmmmm = () => {
     const { params } = useLoaderData<typeof loader>();
     return (
-        <div>
+        <div className={styles.div1}>
             <div>parammmmmm: {params['parammmmmm']}</div>
         </div>
     );
